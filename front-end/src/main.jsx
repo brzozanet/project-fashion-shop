a687c1f7e4d758e3b04a5473b09b10350a3d7a25
Paweł Brzozowski <pawel@brzoza.net>
2025-03-19 23:16:49 +0100

    Refactor App component to export and integrate with React Router for improved routing

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,6 +1,18 @@
 import "./styles/globals.css";
 import "./styles/theme.css";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { App } from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    path: "/",
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <RouterProvider router={router} />
+  </>
+);
