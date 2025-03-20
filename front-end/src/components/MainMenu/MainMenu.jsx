ea33a52b01421f9a8e83840edc64a4e064c18539
Paweł Brzozowski <pawel@brzoza.net>
2025-03-20 19:12:37 +0100

    Add GENDERS constant and update MainMenu component for dynamic navigation

--- front-end/src/components/MainMenu/MainMenu.jsx
+++ front-end/src/components/MainMenu/MainMenu.jsx
@@ -1,9 +1,22 @@
-// import css from "./MainMenu.module.css";
+import { GENDERS } from "../../constans/genders";
+import { NavLink } from "react-router-dom";
+import { nanoid } from "nanoid";
+import css from "./MainMenu.module.css";
 
 export const MainMenu = () => {
   return (
     <>
-      <div>MainMenu</div>
+      <div className={css.container}>
+        <ul className={css.mainMenuList}>
+          {GENDERS.map((category) => {
+            return (
+              <NavLink key={nanoid()} className={css.mainMenuItem}>
+                {category.name}
+              </NavLink>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 };
