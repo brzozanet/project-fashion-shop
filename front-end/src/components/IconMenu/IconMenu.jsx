89a568ef883459e3a03172c946197fea796a6d29
Paweł Brzozowski <pawel@brzoza.net>
2025-03-22 17:12:27 +0100

    Add IconMenu component with icons for favorites and cart, and update styles

--- front-end/src/components/IconMenu/IconMenu.jsx
+++ front-end/src/components/IconMenu/IconMenu.jsx
@@ -1,9 +1,23 @@
-// import css from "./IconMenu.module.css";
+import { Link } from "react-router-dom";
+import HEART_ICON from "../../assets/iconHeart.svg";
+import BAG_ICON from "../../assets/iconBag.svg";
+import css from "./IconMenu.module.css";
 
 export const IconMenu = () => {
   return (
     <>
-      <div>IconMenu</div>
+      <ul className={css.iconMenuList}>
+        <li>
+          <Link to="/ulubione">
+            <img src={HEART_ICON} alt="Ulubione" title="Ulubione"></img>
+          </Link>
+        </li>
+        <li>
+          <Link to="/koszyk">
+            <img src={BAG_ICON} alt="Koszyk" title="Koszyk"></img>
+          </Link>
+        </li>
+      </ul>
     </>
   );
 };
