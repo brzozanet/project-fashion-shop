0f1ad37fe0bb469ad3db8fbbd74c5acef906293c
Paweł Brzozowski <pawel@brzoza.net>
2025-03-24 21:02:58 +0100

    Add CategoriesMenu component with dynamic category links and update styles

--- front-end/src/components/CategoriesMenu/CategoriesMenu.jsx
+++ front-end/src/components/CategoriesMenu/CategoriesMenu.jsx
@@ -1,9 +1,24 @@
+import { CATEGORIES } from "../../constans/categories";
+import { Link } from "react-router-dom";
+import { nanoid } from "nanoid";
 import css from "./CategoriesMenu.module.css";
 
 export const CategoriesMenu = () => {
   return (
     <>
-      <div className={css.categoriesMenu}>CategoriesMenu</div>
+      <div className={css.categoriesMenu}>
+        <ul>
+          {CATEGORIES.map((category) => {
+            return (
+              <li className={css.categoryItem} key={nanoid()}>
+                <Link to={category.path} className={css.categoryLink}>
+                  {category.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 };
---
+++ front-end/src/constans/categories.js
@@ -0,0 +1,18 @@
+export const CATEGORIES = [
+  {
+    name: "Odzież",
+    path: "odziez",
+  },
+  {
+    name: "Obuwie",
+    path: "obuwie",
+  },
+  {
+    name: "Akcesoria",
+    path: "akcesoria",
+  },
+  {
+    name: "Sport",
+    path: "sport",
+  },
+];
