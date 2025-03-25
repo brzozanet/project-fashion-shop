c04be5d138300164ee8303ec74249779c19bee23
Paweł Brzozowski <pawel@brzoza.net>
2025-03-25 15:06:09 +0100

    Remove CenteredContent component and associated styles

--- front-end/src/components/CenteredContent/CenteredContent.jsx
+++
@@ -1,9 +0,0 @@
-import css from "./CenteredContent.module.css";
-
-export const CenteredContent = ({ children }) => {
-  return (
-    <>
-      <div className={css.centeredContent}>{children}</div>
-    </>
-  );
-};
