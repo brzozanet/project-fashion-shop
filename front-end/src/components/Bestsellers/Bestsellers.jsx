28330698fe5952da884b7b5b42659bd2e318bac6
Paweł Brzozowski <pawel@brzoza.net>
2025-03-26 12:47:17 +0100

    Add Product component and update Bestsellers to display products with improved styling

--- front-end/src/components/Bestsellers/Bestsellers.jsx
+++ front-end/src/components/Bestsellers/Bestsellers.jsx
@@ -1,12 +1,92 @@
+import { CenteredContent } from "../CenteredContent/CenteredContent";
+import { Product } from "../Product/Product";
 // import css from "./Bestsellers.module.css"
 
-import { CenteredContent } from "../CenteredContent/CenteredContent";
-
 export const Bestsellers = () => {
+  const bestsellersMockup = [
+    {
+      id: 26,
+      gender: "women",
+      category: "obuwie",
+      subcategory: "eleganckie",
+      productName: "Szpilki",
+      brand: "Test producent",
+      pricePLN: 49,
+      priceUSD: 10,
+      photos: [
+        "http://localhost:3000/product-photos/women-shoes-1.jpg",
+        "http://localhost:3000/product-photos/women-shoes-2.jpg",
+        "http://localhost:3000/product-photos/women-shoes-3.jpg",
+      ],
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
+      maintenanceInfo:
+        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
+    },
+    {
+      id: 27,
+      gender: "women",
+      category: "obuwie",
+      subcategory: "eleganckie",
+      productName: "Szpilki 2",
+      brand: "Inny proucent",
+      pricePLN: 149,
+      priceUSD: 39,
+      photos: [
+        "http://localhost:3000/product-photos/women-shoes-2.jpg",
+        "http://localhost:3000/product-photos/women-shoes-3.jpg",
+        "http://localhost:3000/product-photos/women-shoes-1.jpg",
+      ],
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
+      maintenanceInfo:
+        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
+    },
+    {
+      id: 28,
+      gender: "women",
+      category: "odziez",
+      subcategory: "koszulki",
+      productName: "Biały Sweter",
+      brand: "Sun zi",
+      pricePLN: 299,
+      priceUSD: 59,
+      photos: [
+        "http://localhost:3000/product-photos/women-sweater-1.jpg",
+        "http://localhost:3000/product-photos/women-trousers-2.jpg",
+        "http://localhost:3000/product-photos/women-trousers-1.jpg",
+      ],
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
+      maintenanceInfo:
+        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
+    },
+    {
+      id: 29,
+      gender: "women",
+      category: "odziez",
+      subcategory: "spodnie",
+      productName: "Spodnie",
+      brand: "Shin-tzu",
+      pricePLN: 149,
+      priceUSD: 39,
+      photos: [
+        "http://localhost:3000/product-photos/women-trousers-1.jpg",
+        "http://localhost:3000/product-photos/women-trousers-2.jpg",
+        "http://localhost:3000/product-photos/women-trousers-2.jpg",
+      ],
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
+      maintenanceInfo:
+        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
+    },
+  ];
+
   return (
     <>
       <CenteredContent>
-        <h2>Bestsellers component</h2>
+        <h2>Sprawdź nasze bestellery</h2>
+        <Product />
       </CenteredContent>
     </>
   );
---
+++ front-end/src/components/Product/Product.jsx
@@ -0,0 +1,13 @@
+import css from "./Product.module.css";
+
+export const Product = () => {
+  return (
+    <>
+      <div className={css.product}>
+        <img src="" alt="" title="" />
+        <h3>Product name</h3>
+        <p>Product price</p>
+      </div>
+    </>
+  );
+};
