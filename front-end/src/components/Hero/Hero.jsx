38c5a7a75e683d4bf5dbac5f98aa17e66a5cc77c
Paweł Brzozowski <pawel@brzoza.net>
2025-03-26 11:52:44 +0100

    Add Hero component with promotional content and background image

--- front-end/src/components/Hero/Hero.jsx
+++ front-end/src/components/Hero/Hero.jsx
@@ -1,12 +1,22 @@
 import { CenteredContent } from "../CenteredContent/CenteredContent";
-// import css from "./Hero.module.css";
+import IMAGE_HERO from "../../assets/heroSummersaleFull.jpg";
+import css from "./Hero.module.css";
 
 export const Hero = () => {
   return (
     <>
-      <CenteredContent>
-        <h2>Hero component</h2>
-      </CenteredContent>
+      <div
+        className={css.hero}
+        style={{ backgroundImage: `url(${IMAGE_HERO})` }}
+      >
+        <CenteredContent>
+          <div className={css.promo}>
+            <h2>Letnie promocje do -70%</h2>
+            <p>Tylko najlepsze okazje!</p>
+            <button>Sprawdź produkty</button>
+          </div>
+        </CenteredContent>
+      </div>
     </>
   );
 };
