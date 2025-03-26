f6b29245461654af0948140fc93f7569629a5860
Paweł Brzozowski <pawel@brzoza.net>
2025-03-26 13:58:35 +0100

    Add heart icon to Product component and update styles for improved layout

--- front-end/src/components/Product/Product.jsx
+++ front-end/src/components/Product/Product.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import ICON_HEART from "../../assets/iconHeart.svg";
 import css from "./Product.module.css";
 
 export const Product = ({ name, price, photo }) => {
   return (
     <>
-      <Link to="">
+      <Link to="" className={css.product}>
         <div className={css.productPhotoContainer}>
           <img
             src={photo}
@@ -12,6 +13,7 @@ export const Product = ({ name, price, photo }) => {
             title={name}
             className={css.productPhoto}
           />
+          <img src={ICON_HEART} className={css.heartIcon} />
         </div>
         <h3 className={css.productTitle}>{name}</h3>
         <p className={css.productPrice}>{price} zł</p>
