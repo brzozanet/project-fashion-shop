2b8a2d7b392f1104e3f93c468001b928718c63aa
Paweł Brzozowski <pawel@brzoza.net>
2025-03-26 10:42:42 +0100

    Replace CenteredContent with Hero component in MainPage for enhanced presentation

---
+++ front-end/src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import { CenteredContent } from "../CenteredContent/CenteredContent";
+// import css from "./Hero.module.css";
+
+export const Hero = () => {
+  return (
+    <>
+      <CenteredContent>
+        <h2>Hero component</h2>
+      </CenteredContent>
+    </>
+  );
+};
--- front-end/src/views/MainPage.jsx
+++ front-end/src/views/MainPage.jsx
@@ -1,14 +1,12 @@
 // import css from "./MainPage.module.css";
-import { CenteredContent } from "../components/CenteredContent/CenteredContent";
+import { Hero } from "../components/Hero/Hero";
 import { Layout } from "../components/Layout/Layout";
 
 export const MainPage = () => {
   return (
     <>
       <Layout>
-        <CenteredContent>
-          <h1>MainPage view</h1>
-        </CenteredContent>
+        <Hero />
       </Layout>
     </>
   );
