63200663445ff1b531dfdc2a6892b2d614d76cbc
Paweł Brzozowski <pawel@brzoza.net>
2025-03-27 10:54:21 +0100

    Add delete and add to cart icons to FavouriteProduct component; enhance layout with sub-box styling

--- front-end/src/components/FavouriteProduct/FavouriteProduct.jsx
+++ front-end/src/components/FavouriteProduct/FavouriteProduct.jsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import ICON_DELETE from "../../assets/icon_delete.svg";
+import ICON_CART from "../../assets/icon_cart.svg";
 import css from "./FavouriteProduct.module.css";
 
 export const FavouriteProduct = ({
@@ -32,10 +35,24 @@ export const FavouriteProduct = ({
           </div>
         </div>
         <div className={css.favouriteBox}>
-          <h3 className={css.favouriteTitle}>
-            {brand} {name}
-          </h3>
-          <p>{truncateTextSmart(description, 100)}</p>
+          <div className={css.favouriteSubBox}>
+            <div className={css.favouriteSubBoxText}>
+              <h3 className={css.favouriteTitle}>
+                {brand} {name}
+              </h3>
+              <p>{truncateTextSmart(description, 100)}</p>
+            </div>
+            <div>
+              <Link className={css.favouriteIcon}>
+                <img src={ICON_DELETE} width="14" height="14" />
+                <span className={css.favouriteIconText}>Usuń</span>
+              </Link>
+              <Link className={css.favouriteIcon}>
+                <img src={ICON_CART} width="14" height="14" />
+                <span className={css.favouriteIconText}>Dodaj do koszyka</span>
+              </Link>
+            </div>
+          </div>
         </div>
         <div className={css.favouriteBox}>
           <p className={css.favouritePrice}>{price} zł</p>
