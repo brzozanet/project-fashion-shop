a08a9d84f1208a2d61623e03990604551013f0a6
Paweł Brzozowski <pawel@brzoza.net>
2025-03-28 15:07:53 +0100

    Refactor ExpandableMenu component to use NavLink and rename activeGender variable

--- front-end/src/components/ExpandableMenu/ExpandableMenu.jsx
+++ front-end/src/components/ExpandableMenu/ExpandableMenu.jsx
@@ -1,24 +1,25 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { CATEGORIES } from "../../constans/categories";
 import { nanoid } from "nanoid";
 import ICON_ARROW from "../../assets/icon_arrow.svg";
 import css from "./ExpandableMenu.module.css";
 
 export const ExpandableMenu = () => {
-  const activeGender = "Kobieta";
+  const activeGenderName = "Kobieta";
+  // const activeCategoryPath = "odziez";
 
   return (
     <>
       <div className={css.expandableMenu}>
-        <p className={css.genderTitle}>{activeGender}</p>
+        <p className={css.genderTitle}>{activeGenderName}</p>
         <div>
           <ul>
             {CATEGORIES.map((category) => {
               return (
                 <li key={nanoid()} className={css.categoriesItem}>
-                  <Link to={category.path} className={css.categoriesLink}>
+                  <NavLink to={category.path} className={css.categoriesLink}>
                     {category.name} <img src={ICON_ARROW} />
-                  </Link>
+                  </NavLink>
                 </li>
               );
             })}
