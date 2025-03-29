81dff5ad23a6aad280a196bee15dbe7db1bb1074
Paweł Brzozowski <pawel@brzoza.net>
2025-03-29 16:05:33 +0100

    Refactor Breadcrumbs component to use NavLink and implement dynamic breadcrumb items

--- front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
+++ front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,11 +1,30 @@
+import { NavLink } from "react-router-dom";
+import { nanoid } from "nanoid";
+import ICON_ARROW from "../../assets/icon_arrow.svg";
+
 import css from "./Breadcrumbs.module.css";
 
 export const Breadcrumbs = () => {
+  const breadcrumbsMockup = [
+    { name: "Kobieta", path: "kobieta" },
+    { name: "Odzież", path: "odziez" },
+    { name: "Swetry", path: "swetry" },
+  ];
+
   return (
     <>
-      <div className={css.breadcrumbs}>
-        Kobieta {">"} Odzież {">"} Swetry
-      </div>
+      <ul className={css.breadcrumbsList}>
+        {breadcrumbsMockup.map((breadcrumb) => {
+          return (
+            <li key={nanoid()} className={css.breadcrumbsItem}>
+              <NavLink to={breadcrumb.path} className={css.breadcrumbsLink}>
+                {breadcrumb.name}
+                <img src={ICON_ARROW} className={css.breadcrumbsImage} />
+              </NavLink>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
