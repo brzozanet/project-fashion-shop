aad2f82ea6cb0860ee371f9847820be572c7a373
Paweł Brzozowski <pawel@brzoza.net>
2025-03-29 17:45:01 +0100

    Add mockup data for product details and create Photos component with styling

---
+++ front-end/src/components/Photos/Photos.jsx
@@ -0,0 +1,9 @@
+import css from "./Photos.module.css";
+
+export const Photos = () => {
+  return (
+    <>
+      <div className={css.photos}>Photos</div>
+    </>
+  );
+};
--- front-end/src/views/ProductDetails/ProductDetails.jsx
+++ front-end/src/views/ProductDetails/ProductDetails.jsx
@@ -5,6 +5,28 @@ import { ExpandableMenu } from "../../components/ExpandableMenu/ExpandableMenu";
 import { Breadcrumbs } from "../../components/Breadcrumbs/Breadcrumbs";
 
 export const ProductDetails = () => {
+  const productDetailsMockup = [
+    {
+      id: 28,
+      gender: "women",
+      category: "odziez",
+      subcategory: "koszulki",
+      name: "Biały Sweter",
+      brand: "Sun zi",
+      pricePLN: 99,
+      priceEUR: 59,
+      photos: [
+        "http://localhost:3000/product-photos/women-sweater-1.jpg",
+        "http://localhost:3000/product-photos/women-trousers-2.jpg",
+        "http://localhost:3000/product-photos/women-trousers-1.jpg",
+      ],
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
+      maintenanceInfo:
+        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
+    },
+  ];
+
   return (
     <>
       <Layout>
