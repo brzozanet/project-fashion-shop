a479b4141362c5eb508ab35a35c2e63e670d3a8e
Paweł Brzozowski <pawel@brzoza.net>
2025-03-29 18:22:41 +0100

    Add Photos component with dynamic photo selection and styling

--- front-end/src/components/Photos/Photos.jsx
+++ front-end/src/components/Photos/Photos.jsx
@@ -1,9 +1,38 @@
+import { Link } from "react-router-dom";
+import { nanoid } from "nanoid";
 import css from "./Photos.module.css";
+import { useState } from "react";
 
-export const Photos = () => {
+export const Photos = ({ photos, name }) => {
+  const [selectedPhoto, setSelectedPhoto] = useState(0);
+
   return (
     <>
-      <div className={css.photos}>Photos</div>
+      <div className={css.photos}>
+        <div>
+          {photos.map((photo, index) => {
+            return (
+              <li key={nanoid()}>
+                <Link
+                  to=""
+                  onClick={() => setSelectedPhoto(index)}
+                  className={css.photoThumbnailLink}
+                >
+                  <img
+                    src={photo}
+                    alt={name}
+                    title={name}
+                    className={css.photoThumbnailImg}
+                  />
+                </Link>
+              </li>
+            );
+          })}
+        </div>
+        <div className={css.photoBigContainer}>
+          <img src={photos[selectedPhoto]} className={css.photoBigImg} />
+        </div>
+      </div>
     </>
   );
 };
--- front-end/src/views/ProductDetails/ProductDetails.jsx
+++ front-end/src/views/ProductDetails/ProductDetails.jsx
@@ -3,29 +3,28 @@ import { CenteredContent } from "../../components/CenteredContent/CenteredConten
 import { FlexContainer } from "../../components/FlexContainer/FlexContainer";
 import { ExpandableMenu } from "../../components/ExpandableMenu/ExpandableMenu";
 import { Breadcrumbs } from "../../components/Breadcrumbs/Breadcrumbs";
+import { Photos } from "../../components/Photos/Photos";
 
 export const ProductDetails = () => {
-  const productDetailsMockup = [
-    {
-      id: 28,
-      gender: "women",
-      category: "odziez",
-      subcategory: "koszulki",
-      name: "Biały Sweter",
-      brand: "Sun zi",
-      pricePLN: 99,
-      priceEUR: 59,
-      photos: [
-        "http://localhost:3000/product-photos/women-sweater-1.jpg",
-        "http://localhost:3000/product-photos/women-trousers-2.jpg",
-        "http://localhost:3000/product-photos/women-trousers-1.jpg",
-      ],
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
-      maintenanceInfo:
-        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
-    },
-  ];
+  const productDetailsMockup = {
+    id: 28,
+    gender: "women",
+    category: "odziez",
+    subcategory: "koszulki",
+    name: "Biały Sweter",
+    brand: "Sun zi",
+    pricePLN: 99,
+    priceEUR: 59,
+    photos: [
+      "http://localhost:3000/product-photos/women-sweater-1.jpg",
+      "http://localhost:3000/product-photos/women-trousers-1.jpg",
+      "http://localhost:3000/product-photos/women-shoes-1.jpg",
+    ],
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
+    maintenanceInfo:
+      "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
+  };
 
   return (
     <>
@@ -35,7 +34,10 @@ export const ProductDetails = () => {
             <ExpandableMenu />
             <div>
               <Breadcrumbs />
-              <h2>Product Details</h2>
+              <Photos
+                photos={productDetailsMockup.photos}
+                name={productDetailsMockup.name}
+              />
             </div>
           </FlexContainer>
         </CenteredContent>
