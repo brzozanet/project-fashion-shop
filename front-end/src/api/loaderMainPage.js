7c5ec32c4b45ec0150c963b2e0cfc69d8bf7117c
Paweł Brzozowski <pawel@brzoza.net>
2025-04-03 21:21:54 +0200

    Remove unused import of GENDERS_MAPPING in loaderMainPage.js

--- front-end/src/api/loaderMainPage.js
+++ front-end/src/api/loaderMainPage.js
@@ -3,7 +3,6 @@ import { GENDERS_MAPPING } from "../constans/mappings";
 
 export const loaderMainPage = ({ params }) => {
   const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-
   const gender = GENDERS_MAPPING.get(params.gender);
 
   if (gender) {
