a5a7c6b7c11b8bb2e9f66481c3fea5e6e89fe296
Paweł Brzozowski <pawel@brzoza.net>
2025-04-06 14:30:43 +0200

    Refactor: clean up loaderProductList by removing console logs and fixing redirect logic for gender and category

--- front-end/src/api/loaderProductsList.js
+++ front-end/src/api/loaderProductsList.js
@@ -5,6 +5,8 @@ import { CATEGORIES } from "../constans/categories";
 export const loaderProductList = ({ params }) => {
   const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
   const gender = GENDERS_MAPPING.get(params.gender);
+  console.log(params.gender);
+  console.log(params.category);
 
   const categoryExist = CATEGORIES.find((category) => {
     return category.path === params.category;
@@ -29,13 +31,12 @@ export const loaderProductList = ({ params }) => {
       console.log(subcategoryExist.path);
       console.log(params.subcategory);
       if (subcategoryExist.path !== params.subcategory) {
-        return redirect(`/${gender}/${params.category}`);
+        return redirect(`/${params.gender}/${params.category}`);
       }
-      // FIXME: not working
     }
 
     return fetch(`${BACKEND_URL}/products?${searchParams.toString()}`);
   }
 
-  return redirect("/kobieta");
+  return redirect(`/${params.gender}`);
 };
