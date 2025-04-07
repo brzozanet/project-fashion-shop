ae61da93c5b0823b6c231750e0fd962a81f0b324
Paweł Brzozowski <pawel@brzoza.net>
2025-04-07 22:37:00 +0200

    Refactor: update Breadcrumbs component to use dynamic gender name from URL parameters

--- front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
+++ front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,20 +1,24 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import { nanoid } from "nanoid";
 import ICON_ARROW from "../../assets/icon_arrow.svg";
+import { GENDERS_TEXT_MAPPING } from "../../constans/mappings";
 
 import css from "./Breadcrumbs.module.css";
 
 export const Breadcrumbs = () => {
-  const breadcrumbsMockup = [
-    { name: "Kobieta", path: "kobieta" },
+  const params = useParams();
+
+  const activeGenderName = GENDERS_TEXT_MAPPING.get(params.gender);
+
+  const breadcrumbs = [
+    { name: `${activeGenderName}`, path: `/${params.gender}` },
     { name: "Odzież", path: "odziez" },
-    { name: "Swetry", path: "swetry" },
   ];
 
   return (
     <>
       <ul className={css.breadcrumbsList}>
-        {breadcrumbsMockup.map((breadcrumb) => {
+        {breadcrumbs.map((breadcrumb) => {
           return (
             <li key={nanoid()} className={css.breadcrumbsItem}>
               <NavLink to={breadcrumb.path} className={css.breadcrumbsLink}>
