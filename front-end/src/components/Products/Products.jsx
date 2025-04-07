979490777729290386b86ee7db39e8149896a083
Paweł Brzozowski <pawel@brzoza.net>
2025-04-07 22:14:47 +0200

    Refactor: update Products component to dynamically set products title based on category and subcategory

--- front-end/src/components/Products/Products.jsx
+++ front-end/src/components/Products/Products.jsx
@@ -1,13 +1,31 @@
 import { nanoid } from "nanoid";
 import { Product } from "../Product/Product";
+import { CATEGORIES } from "../../constans/categories";
 import css from "./Products.module.css";
+import { useParams } from "react-router-dom";
 
 export const Products = ({ data }) => {
-  const activeProductsCategoryMockup = "Swetry";
+  const params = useParams();
+
+  let productsTitle;
+
+  const activeCategory = CATEGORIES.find(
+    (category) => params.category === category.path
+  );
+
+  productsTitle = activeCategory.name;
+
+  if (params.subcategory) {
+    const activeSubcategory = activeCategory.subcategories.find(
+      (subcategory) => params.subcategory === subcategory.path
+    );
+
+    productsTitle = activeSubcategory.name;
+  }
 
   return (
     <>
-      <h2 className={css.productsTitle}>{activeProductsCategoryMockup}</h2>
+      <h2 className={css.productsTitle}>{productsTitle}</h2>
       <div className={css.products}>
         {data.map((product) => {
           return (
