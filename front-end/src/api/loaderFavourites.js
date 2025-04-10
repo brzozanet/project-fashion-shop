85d52bd2438999e1226c98548d034e164ddedb7c
Paweł Brzozowski <pawel@brzoza.net>
2025-04-10 14:24:22 +0200

    Refactor: add comments for future implementation of multiple fetches in loaderFavourites

--- front-end/src/api/loaderFavourites.js
+++ front-end/src/api/loaderFavourites.js
@@ -2,3 +2,8 @@ export const loaderFavourites = () => {
   const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
   return fetch(`${BACKEND_URL}/favourites`);
 };
+
+// FIXME: Two fetches: products & favourites (id)
+// async await
+// const response1 = await...
+// const response2 = await...
