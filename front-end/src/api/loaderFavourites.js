2ba61d704cd85033bcaea9f793a0710dc5a2a087
Paweł Brzozowski <pawel@brzoza.net>
2025-04-12 14:25:45 +0200

    Refactor: clean up loaderFavourites function by removing outdated comments and improving code readability

--- front-end/src/api/loaderFavourites.js
+++ front-end/src/api/loaderFavourites.js
@@ -1,6 +1,16 @@
 export const loaderFavourites = async () => {
   const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-  // FIXME: Two fetches: products & favourites (id)
-  // async await
-  // Promise.all
+
+  const [favourites, products] = await Promise.all([
+    (await fetch(`${BACKEND_URL}/favourites`)).json(),
+    (await fetch(`${BACKEND_URL}/products`)).json(),
+  ]);
+
+  console.log(favourites);
+  console.log(products);
+
+  const userFavourites = products.filter((product) =>
+    favourites.some((favourite) => favourite.productId === product.id)
+  );
+  console.log(userFavourites);
 };
