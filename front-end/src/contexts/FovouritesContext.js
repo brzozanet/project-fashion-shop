47d45655f31f7b4b5ccc5192cbddf5707a09ba0e
Paweł Brzozowski <pawel@brzoza.net>
2025-06-14 15:41:45 +0200

    Dodano kontekst ulubionych przedmiotów z użyciem useState, umożliwiający zarządzanie ulubionymi w aplikacji.

--- front-end/src/contexts/FovouritesContext.js
+++ front-end/src/contexts/FovouritesContext.js
@@ -1,3 +1,16 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-export const FavouritesContext = createContext([]);
+export const FavouritesContext = createContext({
+  favourites: [],
+  setFavourites: () => {},
+});
+
+export const FavouritesProvider = ({ children }) => {
+  const [favourites, setFavourites] = useState([]);
+
+  return (
+    <FavouritesContext.Provider value={{ favourites, setFavourites }}>
+      {children}
+    </FavouritesContext.Provider>
+  );
+};
