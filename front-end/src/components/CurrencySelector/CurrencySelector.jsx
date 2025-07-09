541d1a5a212d051272170bd0564a6e3f189a786b
Paweł Brzozowski <pawel@brzoza.net>
2025-07-09 23:12:16 +0200

    Dodano kontekst waluty oraz zaktualizowano komponent CurrencySelector, aby obsługiwał zmianę waluty. W komponencie Layout wprowadzono stan waluty i przekazano funkcję setCurrency do CurrencySelector.

--- front-end/src/components/CurrencySelector/CurrencySelector.jsx
+++ front-end/src/components/CurrencySelector/CurrencySelector.jsx
@@ -2,10 +2,14 @@ import { nanoid } from "nanoid";
 import { CURRENCIES } from "../../constans/curriencies";
 import css from "./CurrencySelector.module.css";
 
-export const CurrencySelector = () => {
+export const CurrencySelector = ({ setCurrency }) => {
+  const handleCurrencyChange = (event) => {
+    setCurrency(event.target.value);
+  };
+
   return (
     <>
-      <select className={css.currencySelector}>
+      <select className={css.currencySelector} onChange={handleCurrencyChange}>
         {CURRENCIES.map((currency) => {
           return (
             <option value={currency.name} key={nanoid()}>
--- front-end/src/components/Layout/Layout.jsx
+++ front-end/src/components/Layout/Layout.jsx
@@ -7,21 +7,29 @@ import { Logo } from "../Logo/Logo";
 import { MainContent } from "../MainContent/MainContent";
 import { MainMenu } from "../MainMenu/MainMenu";
 import { TopBar } from "../TopBar/TopBar";
+import { CurrencyContext } from "../../contexts/currencyContext";
+import { useState } from "react";
 
 export const Layout = () => {
+  const [currency, setCurrency] = useState("PLN");
+
+  console.log(currency);
+
   return (
     <>
       <MainContent>
-        <TopBar>
-          <MainMenu />
-          <Logo />
-          <div>
-            <CurrencySelector />
-            <IconMenu />
-          </div>
-        </TopBar>
-        <CategoriesMenu />
-        <Outlet />
+        <CurrencyContext value={[currency, setCurrency]}>
+          <TopBar>
+            <MainMenu />
+            <Logo />
+            <div>
+              <CurrencySelector setCurrency={setCurrency} />
+              <IconMenu />
+            </div>
+          </TopBar>
+          <CategoriesMenu />
+          <Outlet />
+        </CurrencyContext>
       </MainContent>
       <Footer />
     </>
