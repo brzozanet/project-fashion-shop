725240bbd4ccdc806f6542f6df553698ea2398b6
Paweł Brzozowski <pawel@brzoza.net>
2025-07-09 10:47:51 +0200

    Aktualizacja pliku db.json poprzez zamianę wartości productId dla ulubionych przedmiotów oraz dodanie nowego ulubionego przedmiotu. W komponencie Bestsellers poprawiono logikę wyświetlania ulubionych produktów, w tym dodano opcjonalne łańcuchowanie dla identyfikatora ulubionego produktu.

--- front-end/src/components/Bestsellers/Bestsellers.jsx
+++ front-end/src/components/Bestsellers/Bestsellers.jsx
@@ -4,9 +4,6 @@ import { PageTitle } from "../PageTitle/PageTitle";
 import css from "./Bestsellers.module.css";
 
 export const Bestsellers = ({ bestsellersData, favouritesData }) => {
-  console.log(bestsellersData);
-  console.log(favouritesData);
-
   return (
     <>
       <div className={css.bestsellers}>
@@ -14,9 +11,15 @@ export const Bestsellers = ({ bestsellersData, favouritesData }) => {
           <PageTitle>Sprawdź nasze bestellery</PageTitle>
           <div className={css.bestsellersList}>
             {bestsellersData.map((product) => {
+              // Znajdź odpowiedni rekord z favourites dla tego produktu
+              const favouriteRecord = favouritesData.find(
+                (favourite) => favourite.productId === product.id
+              );
               return (
                 <Product
                   id={product.id}
+                  // NOTE: optional chaining
+                  isProductInFavourites={favouriteRecord?.productId}
                   name={product.name}
                   price={product.pricePLN}
                   photo={product.photos[0]}
