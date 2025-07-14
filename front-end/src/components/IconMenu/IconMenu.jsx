0f4f5c3ae05857d486f7b99c0fa62851c6cb3d3a
Paweł Brzozowski <pawel@brzoza.net>
2025-07-14 11:13:09 +0200

    Usunięcie mockupów produktów z komponentu IconMenu oraz dodanie kontekstu CartContext do zarządzania stanem koszyka. Zaktualizowano wyświetlanie liczby produktów w koszyku.

--- front-end/src/components/IconMenu/IconMenu.jsx
+++ front-end/src/components/IconMenu/IconMenu.jsx
@@ -1,87 +1,12 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/CartContext";
 import ICON_HEART from "../../assets/icon_heart.svg";
 import ICON_BAG from "../../assets/icon_cart.svg";
 import css from "./IconMenu.module.css";
 
 export const IconMenu = () => {
-  const cartProductsMockup = [
-    {
-      id: 26,
-      gender: "women",
-      category: "obuwie",
-      subcategory: "eleganckie",
-      name: "Szpilki",
-      brand: "Test producent",
-      pricePLN: 49,
-      priceEUR: 10,
-      photos: [
-        "http://localhost:3000/product-photos/women-shoes-1.jpg",
-        "http://localhost:3000/product-photos/women-shoes-2.jpg",
-        "http://localhost:3000/product-photos/women-shoes-3.jpg",
-      ],
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
-      maintenanceInfo:
-        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
-    },
-    {
-      id: 27,
-      gender: "women",
-      category: "obuwie",
-      subcategory: "eleganckie",
-      name: "Szpilki 2",
-      brand: "Inny proucent",
-      pricePLN: 149,
-      priceEUR: 39,
-      photos: [
-        "http://localhost:3000/product-photos/women-shoes-2.jpg",
-        "http://localhost:3000/product-photos/women-shoes-3.jpg",
-        "http://localhost:3000/product-photos/women-shoes-1.jpg",
-      ],
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
-      maintenanceInfo:
-        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
-    },
-    {
-      id: 28,
-      gender: "women",
-      category: "odziez",
-      subcategory: "koszulki",
-      name: "Biały Sweter",
-      brand: "Sun zi",
-      pricePLN: 299,
-      priceEUR: 59,
-      photos: [
-        "http://localhost:3000/product-photos/women-sweater-1.jpg",
-        "http://localhost:3000/product-photos/women-trousers-2.jpg",
-        "http://localhost:3000/product-photos/women-trousers-1.jpg",
-      ],
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
-      maintenanceInfo:
-        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
-    },
-    {
-      id: 29,
-      gender: "women",
-      category: "odziez",
-      subcategory: "spodnie",
-      name: "Spodnie",
-      brand: "Shin-tzu",
-      pricePLN: 149,
-      priceEUR: 39,
-      photos: [
-        "http://localhost:3000/product-photos/women-trousers-1.jpg",
-        "http://localhost:3000/product-photos/women-trousers-2.jpg",
-        "http://localhost:3000/product-photos/women-trousers-2.jpg",
-      ],
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
-      maintenanceInfo:
-        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
-    },
-  ];
+  const [shoppingCart] = useContext(CartContext);
 
   return (
     <>
@@ -94,7 +19,7 @@ export const IconMenu = () => {
         <li className={css.iconMenuItem}>
           <Link to="/koszyk">
             <img src={ICON_BAG} alt="Koszyk" title="Koszyk" />
-            <div className={css.iconCounter}>{cartProductsMockup.length}</div>
+            <div className={css.iconCounter}>{shoppingCart.length}</div>
           </Link>
         </li>
       </ul>
